2f3485917515d48e9bb16dcf61692a2e12d23d7e
alkoval <avkaaa@mail.ru>
2024-11-07 21:08:02 +0800

    sql query

---
+++ Sql/ChangeXml.sql
@@ -0,0 +1,14 @@
+﻿PREPARE update_xml AS
+    WITH updated AS (
+    SELECT
+    Id,
+    xmlparse(document REPLACE(
+    xpath($1, FileContent)::text,
+    CONCAT($2, '="', $3, '"')
+    )) AS new_xml
+    FROM Files
+    WHERE Id = $4
+    )
+UPDATE Files
+SET FileContent = (SELECT new_xml FROM updated WHERE Files.Id = updated.Id)
+WHERE Id = $4;
